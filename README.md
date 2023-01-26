7a22c67ce993037c4f2b9f15fbb66d6d457756b0
mujahid shaikh <49784949+devmujahidsk@users.noreply.github.com>
2023-01-26 20:04:10 +0530

    Files uploaded done

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ssr-app-react
+ <!-- client side render app run command -->
+
+run app : npm run start
+
+port : 5000
+
+<!-- server side render app run command  -->
+
+run app: node server/index.js
+
+port: 9000
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h2>Hello World!</h2>
+      <h3>Hi Mujahid</h3>
+      <p>Hello Share Server</p>
+    </div>
+  );
+}
